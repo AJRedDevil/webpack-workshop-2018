ccb84bb223632975d16bc12cc92c9460eda75eae
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-08-23 09:49:49 +0545

    adding limit filesize

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,32 @@ const webpackMerge = require("webpack-merge");
 
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
-module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
-  return webpackMerge(
-    {
-      mode,
-      output: {
-        filename: "bundle.js"
-      },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+module.exports = ({mode, presets} = {
+  mode: "production",
+  presets: []
+}) => {
+  return webpackMerge({
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.jpe?g$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 5000
+              }
+            }
+          ]
+        }
+      ]
     },
-    modeConfig(mode)
-  );
+    output: {
+      filename: "bundle.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin(), new webpack.ProgressPlugin()
+    ]
+  }, modeConfig(mode));
 };
